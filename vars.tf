066624842c1f5828f052ac68d33564b496a403b6
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-10 14:42:21 +0530

    variables -> vars

--- vars.tf
+++ vars.tf
@@ -1,3 +1,100 @@
 variable "REGION" {
   default = "us-east-1"
 }
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0261755bbcb8c4a84"
+    us-east-2 = "ami-0430580de6244e02e"
+    ap-south-1 = "ami-08e5424edfe926b43"
+  }
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "USER" {
+  default = "ubuntu"
+}
+
+variable "PUB_KEY_PATH" {
+  default = "terraLOCK.pub"
+}
+
+variable "PVT_KEY_PATH" {
+  default = "terraLOCK"
+}
+
+variable "MY_IP" {
+  default = "103.5.132.4/32"
+}
+
+variable "VPC_NAME" {
+  default = "terraVPC"
+}
+
+variable "ALL_IPv4" {
+  default = "0.0.0.0/0"
+}
+
+variable "ALL_IPv6" {
+  default = "::/0"
+}
+
+variable "PUB_SUB_1_CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PUB_SUB_2_CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PUB_SUB_3_CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PRIV_SUB_1_CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PRIV_SUB_2_CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PRIV_SUB_3_CIDR" {
+  default = "172.21.6.0/24"
+}
+
+variable "RMQ_USER" {
+  default = "rabbit"
+}
+
+variable "RMQ_PASS" {
+  default = "rabbit123"
+}
+
+variable "DB_NAME" {
+  default = "accounts"
+}
+
+variable "DB_USER" {
+  default = "admin"
+}
+
+variable "DB_PASS" {
+  default = "admin123"
+}
