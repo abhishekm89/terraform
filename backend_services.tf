24cb2ea383b7819fbccba818680f6b0cf570a55f
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-28 23:29:40 +0530

    modified -> resourceTypes

--- backend_services.tf
+++ backend_services.tf
@@ -15,20 +15,20 @@ resource "aws_elasticache_subnet_group" "terraElastiCache-subnetgrp" {
 
 # RDS Backend Instance
 resource "aws_db_instance" "terraRDS-instance" {
-  allocated_storage               = 20
-  storage_type                    = "gp2"
-  engine                          = "mysql"
-  engine_version                  = "5.6.34"
-  instance_class                  = "db.t2.micro"
-  database_name                   = var.DB_NAME
-  master_username                 = var.DB_USER
-  master_password                 = var.DB_PASS
-  db_cluster_parameter_group_name = "default.mysql5.6"
-  publicly_accessible             = false
-  multi_az                        = false
-  skip_final_snapshot             = true
-  db_subnet_group_name            = aws_db_subnet_group.terraRDS-subnetgrp.name
-  vpc_security_group_ids          = [aws_security_group.terraSG-backend.id]
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.6.34"
+  instance_class         = "db.t2.micro"
+  db_name                = var.DB_NAME
+  username               = var.DB_USER
+  password               = var.DB_PASS
+  parameter_group_name   = "default.mysql5.6"
+  publicly_accessible    = false
+  multi_az               = false
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.terraRDS-subnetgrp.name
+  vpc_security_group_ids = [aws_security_group.terraSG-backend.id]
 }
 
 # ElastiCache Backend Cluster
