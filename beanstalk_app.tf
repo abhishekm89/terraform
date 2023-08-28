ce73912ee27d5f8b86ab848099fbe62104320d5d
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-28 17:58:49 +0530

    beanstalk -> application

--- beanstalk_app.tf
+++ beanstalk_app.tf
@@ -1,4 +1,4 @@
 # ElasticBeanStalk Application
-resource "aws_elastic_beanstalk_application" "terraBEANAPP" {
+resource "aws_elastic_beanstalk_application" "terraBEAN-APP" {
   name = "beanstalk-application"
 }
