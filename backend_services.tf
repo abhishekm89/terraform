00c84b546de02b67d5540d0a188b9d22b215bf7e
abhishekm89 <magaji.abhishek@gmail.com>
2023-08-29 11:47:58 +0530

    update -> ParameterGroup [mySQL]

--- backend_services.tf
+++ backend_services.tf
@@ -18,12 +18,12 @@ resource "aws_db_instance" "terraRDS-instance" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.6.34"
+  engine_version         = "5.7.37"
   instance_class         = "db.t2.micro"
   db_name                = var.DB_NAME
   username               = var.DB_USER
   password               = var.DB_PASS
-  parameter_group_name   = "default.mysql5.6"
+  parameter_group_name   = "default.mysql5.7"
   publicly_accessible    = false
   multi_az               = false
   skip_final_snapshot    = true
